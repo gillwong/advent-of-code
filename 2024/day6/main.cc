183108019bb4dbdc443725db3adf197bc912b4c3
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2025-01-11 22:27:17 +0800

    update day6

--- 2024/day6/main.cc
+++ 2024/day6/main.cc
@@ -11,19 +11,30 @@ using std::pair;
 using std::string;
 using Map = Guard::Map;
 
-#ifdef MULTIPLE
+#if defined(AUTO) || defined(MULTI)
 #include <future>
+#include <tuple>
 #include <vector>
 using std::async;
 using std::future;
+using std::get;
 using std::launch;
+using std::tuple;
 using std::vector;
 #endif
 
 int main(int argc, char *argv[]) {
+#ifdef MULTI
+  tuple<ifstream *, ostream *, unsigned int> args =
+      processMultiArgs(argc, argv);
+  ifstream *in = get<0>(args);
+  ostream *out = get<1>(args);
+  unsigned int threads = get<2>(args);
+#else
   pair<ifstream *, ostream *> streams = processArgs(argc, argv);
   ifstream *in = streams.first;
   ostream *out = streams.second;
+#endif
 
   Map map(*in);
   auto start = map.getStart();
@@ -40,7 +51,7 @@ int main(int argc, char *argv[]) {
   }
 
   unsigned int uniqueBlocks = 0;
-#ifdef MULTIPLE
+#if defined(AUTO)
   vector<future<bool>> futures;
   for (const auto &pos : guard.getVisited()) {
     futures.emplace_back(async(
@@ -66,6 +77,43 @@ int main(int argc, char *argv[]) {
     if (future.get()) {
       uniqueBlocks += 1;
     }
+  }
+#elif defined(MULTI)
+  vector<future<unsigned int>> futures;
+  const auto &visited = guard.getVisited();
+
+  unsigned int count = 0;
+  vector<vector<pair<unsigned int, unsigned int>>> groups(threads);
+  for (const auto &pos : visited) {
+    groups[count % threads].emplace_back(pos);
+    count += 1;
+  }
+
+  for (const auto &group : groups) {
+    futures.emplace_back(async(
+        launch::async,
+        [&map, &start](const vector<pair<unsigned int, unsigned int>> &group) {
+          unsigned int partial = 0;
+          for (const auto &pos : group) {
+            Map dup(map);
+            dup.addWall(pos.first, pos.second);
+            Guard sim(dup, start.first, start.second);
+            try {
+              for (;;) {
+                sim.move();
+              }
+            } catch (Guard::Loop &) {
+              partial += 1;
+            } catch (Guard::Leaving &) {
+            }
+          }
+          return partial;
+        },
+        group));
+  }
+
+  for (auto &future : futures) {
+    uniqueBlocks += future.get();
   }
 #else
   for (const auto &pos : guard.getVisited()) {
